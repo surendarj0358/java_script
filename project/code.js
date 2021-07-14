0249170ae8786b29cf93291c6832554b6f2dea25
surendarj0358 <87304961+surendarj0358@users.noreply.github.com>
2021-07-14 12:25:22 +0530

    Head 2

--- project/code.js
+++ project/code.js
@@ -1,11 +1,10 @@
-// Here is a bunch of Lorem Ipsum. You will need it. I recomend using word wrap to view this! View > Toggle Word Wrap
+
 const lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi dictum lorem vel elit laoreet, vitae maximus ligula consectetur. Etiam magna quam, varius at risus eu, rutrum commodo lacus. Maecenas felis sem, consequat non congue non, vestibulum vitae orci. Etiam laoreet ultricies enim, sit amet gravida massa sodales vel. Vivamus tempus neque sed mi elementum ornare eget non lorem. Donec dapibus ex tortor, ut vehicula metus scelerisque eget. Aenean dictum iaculis nulla, vel dapibus justo dignissim quis. Curabitur vitae efficitur neque. Ut vitae orci semper, efficitur dolor eget, porta dui. Cras eu sapien aliquam mauris facilisis ullamcorper. Nullam facilisis sem diam, quis tempor felis interdum ut. Praesent eu nibh arcu. Proin a quam augue. Integer malesuada finibus sem eget consectetur. Aenean non hendrerit purus, quis auctor elit. Duis ullamcorper, ex vitae rutrum ullamcorper, diam justo maximus quam, eget pellentesque metus nunc sit amet arcu. Nunc id eros leo. Praesent ultricies, nunc sit amet finibus posuere, quam magna viverra lectus, non tincidunt tellus mauris sit amet turpis. Nam eleifend quam vel justo pulvinar euismod. Duis ac elit id risus ultricies sollicitudin vitae ac quam. Donec et cursus leo, non dignissim enim. Vestibulum in felis imperdiet, venenatis tortor iaculis, congue augue. Nullam egestas dui nec quam sagittis sollicitudin ac blandit turpis. Nunc in fermentum odio. Morbi sodales mi nibh, ac cursus felis ultricies sed. Quisque at porttitor risus. Aenean a massa in libero venenatis tempus sed in mi. Nullam luctus sapien eget sagittis egestas. Pellentesque eros sem, semper eu molestie in, commodo a lacus. Phasellus pellentesque erat magna, eu facilisis nibh porttitor eget. Ut id mi lobortis, consequat orci ac, vulputate metus. Sed vulputate turpis nec lectus malesuada lacinia. Quisque non mattis eros. Aliquam pellentesque urna vulputate, auctor nibh in, fermentum odio. Suspendisse aliquam neque et aliquam facilisis. Donec sapien felis, molestie vel vestibulum non, sagittis sed felis. Aenean sodales iaculis dolor eget malesuada. Fusce ac libero hendrerit, viverra nunc iaculis, maximus nisi. Nulla volutpat nisi purus, non interdum erat hendrerit vitae. Integer viverra dignissim cursus. Phasellus eu libero sed ex faucibus convallis. In et egestas ipsum. Cras a metus mollis, laoreet risus eget, ultrices purus. Duis at ligula venenatis, imperdiet nulla et, bibendum augue. Pellentesque eleifend libero maximus, pretium lectus et, condimentum metus. Nulla vel est diam. In fermentum ut nulla eget finibus. Aliquam at turpis sagittis, feugiat diam ac, pulvinar quam. Phasellus pretium porta aliquet. Nunc sit amet nisl viverra justo porttitor egestas vel vitae eros. Fusce sit amet fringilla sapien. Nunc placerat, est sit amet laoreet pretium, augue turpis lacinia ipsum, non luctus magna ipsum vel sapien. Fusce luctus odio eget nunc rutrum ullamcorper. Integer vel ligula id elit viverra aliquam. Praesent id ultricies neque. Quisque molestie tellus ac ex vulputate rhoncus. Donec nec massa eget augue tincidunt ultrices vitae at diam. Ut risus purus, egestas eu eros sed, egestas auctor erat. Integer vulputate erat nisi, in aliquet turpis faucibus vel. Praesent neque augue, congue at nunc ut, congue ultricies erat. Ut sed erat in dui ornare malesuada. Nam id elementum ex. Vestibulum condimentum erat quis erat molestie hendrerit. Etiam laoreet volutpat quam, eget ornare ex molestie mollis. Phasellus eros mi, ultrices vel dui ac, facilisis consectetur nunc. Aliquam erat volutpat. Donec rutrum laoreet iaculis."
 
-// Add a list of colors to this array
+
 const colors = ["blue", "red", "black", "white", "rose"]
 
 
-// Use this object for your random name generator. Enter in a bunch of objects with first and last names. When generating a new random name, try to grab a random first or last name from any object. Don't just take object as a whole.
 const names = [{
         firstName: "raj",
         lastName: "kumar"
@@ -38,7 +37,11 @@ function randomGen(min, max) {
 }
 
 
-// random number 1 - 100
+function randOneToOneHundred() {
+    let newRandomNum = randomGen(1, 100)
+    document.getElementById("p1").innerHTML = newRandomNum
+}
+
 
 
 function randOneToOneHundred() {
@@ -46,15 +49,7 @@ function randOneToOneHundred() {
     document.getElementById("p1").innerHTML = newRandomNum
 }
 
-// betweeen Start and End Returns Random
 
-
-function randOneToOneHundred() {
-    let newRandomNum = randomGen(1, 100)
-    document.getElementById("p1").innerHTML = newRandomNum
-}
-
-// start and end
 document.getElementById("b2").addEventListener("change", randStartAndEnd)
 
 function randStartAndEnd() {
@@ -65,18 +60,18 @@ function randStartAndEnd() {
     document.getElementById("p2").innerHTML = newRandomValue
 }
 
-// returns a long Lorem Ipsum String
+
 function loremString() {
     document.getElementById("p3").innerHTML = lorem
 }
 
-// Returns a Lorem Ipsum String based on the number of sentences specified
+
 function loremSentences() {
     let numberOfSentence = document.getElementById("loremSentence").value
 
-    // split array sentence
+   
     let newLoremSentence = lorem.split(",")
-    console.log(newLoremSentence)
+    
 
     let newLorem = ""
     for (let i = 0; i < numberOfSentence; i++) {
@@ -86,11 +81,12 @@ function loremSentences() {
     document.getElementById("p4").innerHTML = newLorem
 
 }
-// Returns a Lorem Ipsum String based on the number of characters specified
+
+
 function loremChars() {
     let numberOfSentence = document.getElementById("loremChar").value
 
-    // split array sentence
+    
     let newLoremCharacters = lorem.split("")
 
 
@@ -103,7 +99,7 @@ function loremChars() {
 
 }
 
-// returns current date
+
 function newDate() {
     let myDate = new Date()
     let year = myDate.getFullYear()
@@ -113,9 +109,9 @@ function newDate() {
 
     document.getElementById("p6").innerHTML = myDate
 
-
 }
-// returns time
+
+
 function newTime() {
     let myTime = new Date()
     let hours = myTime.getHours()
@@ -139,22 +135,20 @@ function newTime() {
     document.getElementById("p7").innerHTML = newMyTime
 }
 
-// convert inches to feet
+
 function convertInchesToFeet() {
     let inches = document.getElementById("inches").value
     let convertoFeet = inches / 12 + "FT"
 
     document.getElementById("p8").innerHTML = convertoFeet
 }
-// convert feet to inches
+
 function convertFeetToInches() {
     let feet = document.getElementById("feet").value
     let convertoInches = feet * 12 + "Inches"
 
     document.getElementById("p9").innerHTML = convertoInches
 }
-
-// compares if two words are the same length
 
 function compareWords() {
     let word1 = document.getElementById("firstWord").value
@@ -163,15 +157,14 @@ function compareWords() {
 
     if (word1.length == word2.length) {
 
-        msg = "Both words are the same length"
+        msg = "size of the two string values is same"
     } else {
-        msg = "word lengths are different. word1: " + word1.length + " word2: " + word.length
+        msg = "size of the two string values is different. word1: " + word1.length + " word2: " + word2.length
     }
     document.getElementById("p10").innerHTML = msg
 
 }
 
-// Return a random password
 function passwordGen() {
     let chars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "!", "@", "#"]
 
@@ -183,7 +176,7 @@ function passwordGen() {
     }
     document.getElementById("p11").innerHTML = newPassword
 }
-// number is even or not
+
 function evenOrNot() {
     let newNum = document.getElementById("num").value
     let newMsg = ""
@@ -197,14 +190,14 @@ function evenOrNot() {
     document.getElementById("p12").innerHTML = newMsg
 
 }
-// Returns a random colour
+
 function randomColour() {
     let newColour = colors[randomGen(0, colors.length - 1)]
 
     document.getElementById("p13").innerHTML = newColour
 }
 
-// returns random first and last name
+
 
 function randNames() {
 
